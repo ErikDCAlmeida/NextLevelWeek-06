55888d043a457c5ca0aa865708ffa1702f7d3a8b
Erik Danilo <eriikd@hotmail.com>
2021-06-23 11:29:16 -0300

    Autenticação do Usuário e Criar nova sala.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { BrowserRouter, Route } from "react-router-dom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+
 function App() {
-  return (
-    <h1>Hello World!</h1>
+   return (
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/room/new" component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
